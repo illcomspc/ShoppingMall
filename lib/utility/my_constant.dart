4acbf11103f0e81206e092d88939466bac068985
Jeun <jeun@spc.com>
2021-09-24 21:55:44 +0700

    update

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  // General
+  static String appName = 'Shopping Mall';
+
+  // Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = 'buyerService';
+  static String routeSalerService = 'salerService';
+  static String routeRiderService = 'riderService';
+
+  // Images
+
+  static String image1 = 'images/image1.png';
+  static String image2 = 'images/image2.png';
+  static String image3 = 'images/image3.png';
+  static String image4 = 'images/image4.png';
+
+  // Color
+
+  static Color primary = Color(0xff9e9c23);
+  static Color dark = Color(0xff6c6e00);
+  static Color light = Color(0xffd2cd55);
+
+  // Style
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle h2Style() => TextStyle(
+        fontSize: 18,
+        color: dark,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle h3Style() => TextStyle(
+        fontSize: 14,
+        color: dark,
+        fontWeight: FontWeight.normal,
+      );
+
+  ButtonStyle myButtonStyle() => ElevatedButton.styleFrom(
+        primary: MyConstant.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      );
+}
